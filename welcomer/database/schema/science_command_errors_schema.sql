f2b0b96bd9a5deb6a3080553cd59d7b6233b428b
root <blane1245@hotmail.co.uk>
2022-10-03 23:21:33 +0200

    Fix incorrect column type

--- welcomer/database/schema/science_command_errors_schema.sql
+++ welcomer/database/schema/science_command_errors_schema.sql
@@ -1,8 +1,7 @@
 CREATE TABLE IF NOT EXISTS science_command_errors (
     command_uuid uuid NOT NULL UNIQUE PRIMARY KEY,
-    created_at datetime NOT NULL,
+    created_at timestamp NOT NULL,
     trace text NOT NULL,
     data jsonb NOT NULL,
     FOREIGN KEY (command_uuid) REFERENCES science_command_usages (command_uuid) ON DELETE CASCADE ON UPDATE CASCADE
 );
-
