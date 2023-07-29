8454382f3c5df84bf50cb5ab964aeaedae3e4662
root <blane1245@hotmail.co.uk>
2023-07-29 23:27:23 +0000

    Remove unnecessary columns from guilds table

--- welcomer-core/database/queries/guilds_query.sql
+++ welcomer-core/database/queries/guilds_query.sql
@@ -1,20 +1,18 @@
 -- name: CreateGuild :one
-INSERT INTO guilds (guild_id, created_at, updated_at, name, embed_colour, site_splash_url, site_staff_visible, site_guild_visible, site_allow_invites)
-    VALUES ($1, now(), now(), $2, $3, $4, $5, $6, $7)
+INSERT INTO guilds (guild_id, embed_colour, site_splash_url, site_staff_visible, site_guild_visible, site_allow_invites)
+    VALUES ($1, $2, $3, $4, $5, $6)
 RETURNING
     *;
 
 -- name: CreateOrUpdateGuild :one
-INSERT INTO guilds (guild_id, created_at, updated_at, name, embed_colour, site_splash_url, site_staff_visible, site_guild_visible, site_allow_invites)
-    VALUES ($1, now(), now(), $2, $3, $4, $5, $6, $7)
+INSERT INTO guilds (guild_id, embed_colour, site_splash_url, site_staff_visible, site_guild_visible, site_allow_invites)
+    VALUES ($1, $2, $3, $4, $5, $6)
 ON CONFLICT(guild_id) DO UPDATE
-    SET name = EXCLUDED.name,
-        embed_colour = EXCLUDED.embed_colour,
+    SET embed_colour = EXCLUDED.embed_colour,
         site_splash_url = EXCLUDED.site_splash_url,
         site_staff_visible = EXCLUDED.site_staff_visible,
         site_guild_visible = EXCLUDED.site_guild_visible,
-        site_allow_invites = EXCLUDED.site_allow_invites,
-        updated_at = now()
+        site_allow_invites = EXCLUDED.site_allow_invites
 RETURNING
     *;
 
@@ -30,13 +28,11 @@ WHERE
 UPDATE
     guilds
 SET
-    name = $2,
     embed_colour = $3,
     site_splash_url = $4,
     site_staff_visible = $5,
     site_guild_visible = $6,
-    site_allow_invites = $7,
-    updated_at = now()
+    site_allow_invites = $7
 WHERE
     guild_id = $1;
 
--- welcomer-core/database/schema/guilds_schema.sql
+++ welcomer-core/database/schema/guilds_schema.sql
@@ -1,8 +1,5 @@
 CREATE TABLE IF NOT EXISTS guilds (
     guild_id bigint NOT NULL UNIQUE PRIMARY KEY,
-    created_at timestamp NOT NULL,
-    updated_at timestamp NOT NULL,
-    name text NOT NULL,
     embed_colour integer NOT NULL,
     site_splash_url text NOT NULL,
     site_staff_visible boolean NOT NULL,
