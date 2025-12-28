d6ac6b509112889be57ad5d1e4c944686fb064a9
Blane Thompson <rock@welcomer.gg>
2025-12-28 14:40:07 +0000

    Added solid:profile support

--- welcomer-images-next/service/styling.go
+++ welcomer-images-next/service/styling.go
@@ -1,10 +1,28 @@
 package service
 
 import (
+	"context"
+	"fmt"
+	"image"
+	"image/color"
+	"net/http"
+	"net/url"
 	"strings"
+
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
 
 	"github.com/WelcomerTeam/Welcomer/welcomer-core"
 )
+
+const SolidProfileLuminance = 0.7
+
+type ImageGenerationContext struct {
+	context.Context
+
+	GenerateRequest
+}
 
 type Styling map[string]string
 
@@ -22,7 +40,7 @@ func (s Styling) Build(builder *strings.Builder) {
 	}
 }
 
-func getCanvasStyle(customImage welcomer.CustomWelcomerImage) Styling {
+func (is *ImageService) getCanvasStyle(ctx *ImageGenerationContext, customImage welcomer.CustomWelcomerImage) Styling {
 	styling := Styling{}
 
 	if len(customImage.Dimensions) == 2 {
@@ -38,13 +56,13 @@ func getCanvasStyle(customImage welcomer.CustomWelcomerImage) Styling {
 		styling.Add("height", welcomer.Itoa(int64(customImage.Dimensions[1]))+"px")
 	}
 
-	styling.Add("background", getFillAsCSS(customImage.Fill, "transparent"))
+	styling.Add("background", is.getFillAsCSS(ctx, customImage.Fill, "transparent"))
 	styling.Add("box-sizing", "border-box")
 	styling.Add("overflow", "hidden")
 	styling.Add("position", "relative")
 
 	if customImage.Stroke != nil && customImage.Stroke.Width > 0 {
-		styling.Add("border", welcomer.Itoa(int64(customImage.Stroke.Width))+"px solid "+getFillAsCSS(customImage.Stroke.Color, "#000000"))
+		styling.Add("border", welcomer.Itoa(int64(customImage.Stroke.Width))+"px solid "+is.getFillAsCSS(ctx, customImage.Stroke.Color, "#000000"))
 	}
 
 	return styling
@@ -73,8 +91,6 @@ func getObjectStyleBase(layer welcomer.CustomWelcomerImageLayer, layer_count, in
 		styling.Add("top", "0px")
 	}
 
-	styling.Add("border", "red 1px solid")
-
 	styling.Add(
 		"transform",
 		welcomer.If(layer.Rotation != 0, "rotate("+welcomer.Itoa(int64(layer.Rotation))+"deg)", "")+
@@ -84,7 +100,7 @@ func getObjectStyleBase(layer welcomer.CustomWelcomerImageLayer, layer_count, in
 	return styling
 }
 
-func getObjectStyle(layer welcomer.CustomWelcomerImageLayer, layer_count, index int) Styling {
+func (is *ImageService) getObjectStyle(ctx *ImageGenerationContext, layer welcomer.CustomWelcomerImageLayer, layer_count, index int) Styling {
 	styling := getObjectStyleBase(layer, layer_count, index)
 
 	styling.Add("z-index", "0")
@@ -105,9 +121,9 @@ func getObjectStyle(layer welcomer.CustomWelcomerImageLayer, layer_count, index 
 
 	if layer.Type == welcomer.CustomWelcomerImageLayerTypeText {
 		styling.Add("background", "transparent")
-		styling.Add("color", getFillAsCSS(layer.Fill, "inherit"))
+		styling.Add("color", is.getFillAsCSS(ctx, layer.Fill, "inherit"))
 	} else {
-		styling.Add("background", getFillAsCSS(layer.Fill, "transparent"))
+		styling.Add("background", is.getFillAsCSS(ctx, layer.Fill, "transparent"))
 		styling.Add("color", "inherit")
 	}
 
@@ -135,7 +151,7 @@ func getObjectStyle(layer welcomer.CustomWelcomerImageLayer, layer_count, index 
 			}
 
 			if layer.Stroke.Width != 0 {
-				styling.Add("text-shadow", generateTextShadow(layer.Stroke.Width, getFillAsCSS(layer.Stroke.Color, "#000000")))
+				styling.Add("text-shadow", generateTextShadow(layer.Stroke.Width, is.getFillAsCSS(ctx, layer.Stroke.Color, "#000000")))
 			}
 
 			styling.Add("justify-content", string(layer.Typography.HorizontalAlignment))
@@ -146,14 +162,14 @@ func getObjectStyle(layer welcomer.CustomWelcomerImageLayer, layer_count, index 
 		styling.Add("white-space", "pre-wrap")
 	} else {
 		if layer.Stroke.Width > 0 {
-			styling.Add("border", welcomer.Itoa(int64(layer.Stroke.Width))+"px solid "+getFillAsCSS(layer.Stroke.Color, "#000000"))
+			styling.Add("border", welcomer.Itoa(int64(layer.Stroke.Width))+"px solid "+is.getFillAsCSS(ctx, layer.Stroke.Color, "#000000"))
 		}
 	}
 
 	return styling
 }
 
-func getFillAsCSS(value, defaultValue string) string {
+func (is *ImageService) getFillAsCSS(ctx *ImageGenerationContext, value, defaultValue string) string {
 	if len(value) == 0 {
 		return defaultValue
 	}
@@ -163,21 +179,135 @@ func getFillAsCSS(value, defaultValue string) string {
 	}
 
 	if value == "solid:profile" {
-		// TODO: hex from avatar
+		if ctx.User.Avatar != "" {
+			if src, err := is.fetchImageFromURL(ctx.Context, welcomer.GetUserAvatar(&ctx.User)); err == nil {
+				return getImageLumainceAsHex(src)
+			}
+		}
+
 		return defaultValue
 	}
 
 	if value[:4] == "ref:" {
-		// TODO: artifact serve
-		return defaultValue
+		return "url(https://www.welcomer.gg/api/guild/" + ctx.Guild.ID.String() + "/welcomer/artifact/" + url.QueryEscape(value[4:]) + ")"
 	}
 
 	if _, ok := backgrounds[value]; ok {
-		// TODO: default image serve
-		return defaultValue
+		return "url(https://www.welcomer.gg/assets/backgrounds/" + value + ".webp)"
 	}
 
 	return defaultValue
+}
+
+func (is *ImageService) fetchImageFromURL(ctx context.Context, avatarURL string) (image.Image, error) {
+	parsedURL, isValidURL := welcomer.IsValidURL(avatarURL)
+	if parsedURL == nil || !isValidURL {
+		return nil, welcomer.ErrInvalidURL
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, avatarURL, nil)
+	if err != nil {
+		welcomer.Logger.Error().Err(err).Str("url", avatarURL).Msg("Failed to create new request for avatar")
+
+		return nil, fmt.Errorf("failed to create request: %w", err)
+	}
+
+	resp, err := is.Client.Do(req)
+	if err != nil || resp == nil {
+		welcomer.Logger.Error().Err(err).Str("url", avatarURL).Msg("Failed to fetch profile picture for avatar")
+
+		return nil, fmt.Errorf("failed to fetch image: %w", err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 400 {
+		welcomer.Logger.Error().Err(err).Int("status", resp.StatusCode).Str("url", avatarURL).Msg("Failed to fetch profile picture for avatar")
+
+		return nil, fmt.Errorf("failed to fetch image, status code %d", resp.StatusCode)
+	}
+
+	im, _, err := image.Decode(resp.Body)
+	if err != nil || im == nil {
+		welcomer.Logger.Error().Err(err).Msg("Failed to decode profile picture of user")
+
+		return nil, fmt.Errorf("failed to decode image: %w", err)
+	}
+
+	return im, nil
+}
+
+func getImageLumainceAsHex(src image.Image) string {
+	// Initial threshold for solid profile luminance
+	threshold := SolidProfileLuminance
+
+	var colour color.RGBA
+	var ok bool
+
+	// Iterate through thresholds to find the primary color
+	for threshold > 0 {
+		// Attempt to identify the most common light color based on the current threshold
+		colour, ok = getCommonLuminance(src, threshold)
+
+		// If not found, decrease the threshold and try again
+		if !ok {
+			threshold -= 0.1
+		} else {
+			break // Exit the loop if a color is found
+		}
+	}
+
+	return fmt.Sprintf("#%02x%02x%02x", colour.R, colour.G, colour.B)
+}
+
+// getCommonLuminance calculates the most common light color in the image.
+// It takes an image.Image 'src' and a 'threshold' for defining lightness.
+// It returns the most common light color as a color.RGBA value and a boolean flag ('ok') indicating success.
+func getCommonLuminance(src image.Image, threshold float64) (colour color.RGBA, ok bool) {
+	// Map to store the count of each color with its occurrence
+	colorCount := make(map[color.RGBA]int)
+
+	// Traverse through each pixel in the image
+	bounds := src.Bounds()
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			// Extract RGB values of the pixel
+			r, g, b, a := src.At(x, y).RGBA()
+
+			// Check if the color is considered light based on the provided threshold
+			if a > 0 && isLightColorLuminance(r, g, b, threshold) {
+				// Store the color in the map and count its occurrences
+				colorCount[color.RGBA{uint8(r), uint8(g), uint8(b), 255}]++
+			}
+		}
+	}
+
+	// If no light color found, return default black color and set 'ok' flag to false
+	if len(colorCount) == 0 {
+		return colour, false
+	}
+
+	// Find the most common light color by counting occurrences
+	maxOccurrences := 0
+	for color, count := range colorCount {
+		if count > maxOccurrences {
+			colour = color
+			maxOccurrences = count
+		}
+	}
+
+	return colour, true
+}
+
+// isLightColorLuminance determines if a color is considered 'light' based on its luminance and a threshold value.
+// It takes the red (r), green (g), blue (b) values and a threshold for luminance.
+// It returns true if the color is light, false otherwise.
+func isLightColorLuminance(r, g, b uint32, threshold float64) bool {
+	// Calculate luminance of the color using RGB values
+	lum := 0.299*float64(r) + 0.587*float64(g) + 0.114*float64(b)
+
+	// Check if the luminance exceeds the threshold
+	return lum > threshold
 }
 
 func normalizeBorderRadius(value string) string {
@@ -204,7 +334,9 @@ func generateTextShadow(width int, color string) string {
 				if !first {
 					builder.WriteString(", ")
 				}
+
 				builder.WriteString(welcomer.Itoa(int64(dx)) + "px " + welcomer.Itoa(int64(dy)) + "px 0 " + color)
+
 				first = false
 			}
 		}
