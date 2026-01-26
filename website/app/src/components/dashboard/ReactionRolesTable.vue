d515f36e18fe9b692dc4a55cafd2942db09df114
Blane Thompson <rock@welcomer.gg>
2026-01-26 20:13:06 +0000

    feat: Added reaction roles table

---
+++ website/app/src/components/dashboard/ReactionRolesTable.vue
@@ -0,0 +1,151 @@
+<template>
+  <div>
+    <table class="min-w-full border-spacing-2">
+      <thead>
+        <tr>
+          <th></th>
+          <th></th>
+          <th class="text-right">
+            <button type="button" class="cta-button bg-primary hover:bg-primary-dark" @click="openCreatePopup()">
+              Create Configuration
+            </button>
+          </th>
+        </tr>
+      </thead>
+      <tbody class="divide-y divide-gray-200 dark:divide-secondary-light">
+        <tr v-for="(reactionRole, index) in this.$props.modelValue.reaction_roles" :key="index">
+          <td class="py-3">
+            <Switch v-model="reactionRole.enabled" :class="[
+                reactionRole.enabled ? 'bg-green-500 focus:ring-green-500' : 'bg-gray-400 focus:ring-gray-400',
+                'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2',
+              ]">
+              <span :class="[
+                reactionRole.enabled ? 'translate-x-5' : 'translate-x-0',
+                'pointer-events-none relative inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200',
+              ]">
+                <span :class="[
+                  reactionRole.enabled
+                    ? 'opacity-0 ease-out duration-100'
+                    : 'opacity-100 ease-in duration-200',
+                  'absolute inset-0 h-full w-full flex items-center justify-center transition-opacity',
+                ]" aria-hidden="true">
+                  <svg class="w-3 h-3 text-gray-400" fill="none" viewBox="0 0 12 12">
+                    <path d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                      stroke-linejoin="round" />
+                  </svg>
+                </span>
+                <span :class="[
+                  reactionRole.enabled
+                    ? 'opacity-100 ease-in duration-200'
+                    : 'opacity-0 ease-out duration-100',
+                  'absolute inset-0 h-full w-full flex items-center justify-center transition-opacity',
+                ]" aria-hidden="true">
+                  <svg class="w-3 h-3 text-green-500" fill="currentColor" viewBox="0 0 12 12">
+                    <path
+                      d="M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z" />
+                  </svg>
+                </span>
+              </span>
+            </Switch>
+          </td>
+          <td class="py-3">
+            <discord-embed class="flex-1" v-if="reactionRole.message" :embeds="reactionRole.is_system_message ? [reactionRole.message] : null" :buttons="getReactionRoleButtons(reactionRole.type, reactionRole.roles)" :isLight="true" :showAuthor="false" />
+          </td>
+          <td class="py-3 text-right">
+            <button class="relative py-2 px-2 border border-gray-300 dark:border-secondary-light rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-primary focus:border-primary sm:text-sm" @click="reactionRole.showPopup = true">
+              <font-awesome-icon icon="pen-to-square" class="w-5 h-5 text-gray-400" aria-hidden="true" />
+            </button>
+          </td>
+          <popup :open="reactionRole.showPopup" @close="reactionRole.showPopup = false">
+            <reaction-roles-table-item :isSetup="false" :reactionRole="reactionRole" />
+          </popup>
+        </tr>
+      </tbody>
+    </table>
+    <popup :open="showCreatePopup" @close="showCreatePopup = false">
+      <reaction-roles-table-item :isSetup="true" :reactionRole="createPopupData" />
+    </popup>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+import { Switch } from "@headlessui/vue";
+
+import Popup from "../Popup.vue";
+
+import DiscordEmbed from "@/components/DiscordEmbed.vue";
+
+import ReactionRolesTableItem from "./ReactionRolesTableItem.vue";
+
+export default {
+  components: {
+    DiscordEmbed,
+    Popup,
+    Switch,
+    ReactionRolesTableItem,
+  },
+  props: {
+    modelValue: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  setup() {
+    let showCreatePopup = ref(false);
+    let createPopupData = ref({});
+
+    return {
+      showCreatePopup,
+      createPopupData,
+    };
+  },
+
+  emits: ["update:modelValue"],
+
+  methods: {
+    openCreatePopup() {
+      this.resetCreatePopup();
+      this.showCreatePopup = true;
+    },
+    resetCreatePopup() {
+      this.createPopupData = {
+        is_system_message: undefined,
+        embed: "{\"embeds\":[{\"description\":\"React below to get roles!\"}]}",
+      }
+    },
+
+    updateValue(value) {
+      this.$emit("update:modelValue", value);
+    },
+
+    getReactionRoleButtons(reactionRoleType, roles) {
+      if (reactionRoleType == "dropdown") {
+        return [{
+          style: 6,
+          options: roles.map((roleOption) => ({
+            label: roleOption.name,
+            description: roleOption.description,
+            emoji: roleOption.emoji,
+          })),
+        }];
+      }
+
+      let buttons = [];
+
+      roles.forEach((roleOption) => {
+        buttons.push({
+          type: roleOption.style,
+          label: roleOption.name,
+          emoji: roleOption.emoji,
+          style: reactionRoleType == "emoji" ? 5 : roleOption.style,
+        });
+      });
+
+      return buttons;
+    }
+  }
+}
+</script>
---
+++ website/app/src/components/dashboard/ReactionRolesTableItem.vue
@@ -0,0 +1,200 @@
+<template>
+    <div>
+      <div v-if="onboardingStep >= 0">
+        <h2 class="text-2xl font-bold mb-8 text-center">Select Message Type</h2>
+        <div class="grid grid-cols-2 space-x-2 mb-4">
+          <button :disabled="!$props.isSetup" @click="$props.reactionRole.is_system_message = false" :class="[$props.reactionRole.is_system_message === false ? ' border-primary' : 'border-gray-300 dark:border-secondary-light', 'p-8 border rounded-lg shadow-sm h-fit mb-4 disabled:bg-secondary-light']">
+            <h2 class="font-bold text-lg">Use an existing message</h2>
+            <span>Use an existing message sent by any user, you cannot configure this message on the dashboard.</span>
+          </button>
+          <button :disabled="!$props.isSetup" @click="$props.reactionRole.is_system_message = true" :class="[$props.reactionRole.is_system_message === true ? ' border-primary' : 'border-gray-300 dark:border-secondary-light', 'p-8 border rounded-lg shadow-sm h-fit mb-4 disabled:bg-secondary-light']">
+            <h2 class="font-bold text-lg">Create New Message</h2>
+            <span>Create a message sent by Welcomer, you can then configure this message on the dashboard.</span>
+          </button>
+        </div>
+
+        <div v-if="$props.reactionRole.is_system_message === false" class="mb-4">
+          <form-value :type="FormTypeText" title="Message Link" v-model="$props.reactionRole.message_link" placeholder="https://discord.com/channels/123456789012345678/123456789012345678/123456789012345678" :validation="v$.message_link">
+            Enter the link to the message you want to use for reaction roles. You can get this by selecting a message and clicking on <b>Copy Message Link</b> on Discord.
+          </form-value>
+        </div>
+        <div v-if="$props.reactionRole.is_system_message === true" class="mb-4">
+          <form-value :type="FormTypeChannelListCategories" title="Channel" v-model="$props.reactionRole.channel_id" :validation="v$.channel_id"/>
+          <form-value :type="FormTypeEmbed" title="Message" v-model="$props.reactionRole.embed" class="mt-4" :validation="v$.embed"/>
+        </div>
+      </div>
+      <div v-if="onboardingStep >= 1">
+        <h2 class="text-2xl font-bold mb-8 mt-8 text-center">Select Reaction Role Type</h2>
+        <div class="grid grid-cols-3 space-x-2 mb-4">
+          <button @click="$props.reactionRole.type = 'emoji'" :class="[$props.reactionRole.type === 'emoji' ? ' border-primary' : 'border-gray-300 dark:border-secondary-light', 'p-8 border rounded-lg shadow-sm h-fit disabled:bg-secondary-light']">
+            <h2 class="font-semibold text-center mb-2">Emojis</h2>
+            <img src="/assets/reaction_roles_emoji.png" alt="Emoji Reaction Roles" class="mt-4" />
+          </button>
+          <button @click="$props.reactionRole.type = 'buttons'" :disabled="$props.reactionRole.is_system_message == false" :class="[$props.reactionRole.type === 'buttons' ? ' border-primary' : 'border-gray-300 dark:border-secondary-light', 'p-8 border rounded-lg shadow-sm h-fit disabled:bg-secondary-light']">
+            <h2 class="font-semibold text-center mb-2">Buttons</h2>
+            <img src="/assets/reaction_roles_buttons.png" alt="Button Reaction Roles" class="mt-4" />
+          </button>
+          <button @click="$props.reactionRole.type = 'dropdown'" :disabled="$props.reactionRole.is_system_message == false" :class="[$props.reactionRole.type === 'dropdown' ? ' border-primary' : 'border-gray-300 dark:border-secondary-light', 'p-8 border rounded-lg shadow-sm h-fit disabled:bg-secondary-light']">
+            <h2 class="font-semibold text-center mb-2">Dropdown</h2>
+            <img src="/assets/reaction_roles_dropdown.png" alt="Dropdown Reaction Roles" class="mt-4" />
+          </button>
+        </div>
+      </div>
+      <div v-if="onboardingStep >= 2">
+        <h2 class="text-2xl font-bold mb-8 mt-8 text-center">Select Roles</h2>
+        
+      </div>
+
+      <div v-if="onboardingStep == 0" class="flex justify-end">
+        <button @click="validateOnboardingStepZero" class="px-4 py-2 bg-blue-600 text-white rounded w-full md:w-auto">
+          <loading-icon class="inline-block mr-2" :isLight="true" v-if="showOnboardingLoading" />
+          Next
+        </button>
+      </div>
+      <div v-else-if="onboardingStep == 1" class="flex justify-end">
+        <button :disabled="!($props.reactionRole.type == 'emoji' || $props.reactionRole.type == 'buttons' || $props.reactionRole.type == 'dropdown')" @click="validateOnboardingStepOne" class="px-4 py-2 bg-blue-600 text-white rounded w-full md:w-auto disabled:opacity-50 disabled:cursor-not-allowed">
+          Next
+        </button>
+      </div>
+    </div>
+</template>
+
+<script>
+import { computed, ref } from "vue";
+
+import useVuelidate from "@vuelidate/core";
+import { helpers, requiredIf } from "@vuelidate/validators";
+
+import FormValue from "@/components/dashboard/FormValue.vue";
+
+import LoadingIcon from "@/components/LoadingIcon.vue";
+
+import EmbedBuilder from "@/components/dashboard/EmbedBuilder.vue";
+
+import {
+  FormTypeChannelListCategories,
+  FormTypeEmbed,
+  FormTypeText
+} from "@/components/dashboard/FormValueEnum";
+
+import {
+  getErrorToast,
+  getValidationToast,
+  navigateToErrors,
+  isValidJson
+} from "@/utilities";
+
+import dashboardAPI from "@/api/dashboard";
+import endpoints from "@/api/endpoints";
+import store from "@/store";
+
+export default {
+  components: {
+    EmbedBuilder,
+    FormValue,
+    LoadingIcon,
+  },
+  props: {
+    isSetup: {
+      type: Boolean,
+      required: true,
+    },
+    reactionRole: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  setup(props) {
+    let onboardingStep = ref(props.reactionRole.is_system_message == undefined ? 0 : 2);
+    let showOnboardingLoading = ref(false);
+    // Onboarding steps:
+    // 0 - Select message
+    // 1 - Select type and roles
+
+    const validation_rules = computed(() => {
+      const validation_rules = {
+        message_link: {
+          required: helpers.withMessage("Message Link is required.", requiredIf(() => props.reactionRole.is_system_message === false)),
+          discordMessageLink: helpers.withMessage("Please enter a valid Discord message link.", (value) => !value || /^https:\/\/discord\.com\/channels\/\d+\/\d+\/\d+$/.test(value)),
+          currentServer: helpers.withMessage("The message link must be for the current server.", (value) => {
+            if (!value) return true;
+            
+            const messageLinkParts = value.split("/");
+            const guildID = messageLinkParts[messageLinkParts.length - 3];
+            console.log(guildID, store.getters.getSelectedGuildID);
+            return guildID == store.getters.getSelectedGuildID;
+          }),
+        },
+        channel_id: {
+          required: helpers.withMessage("Channel is required.", requiredIf(
+            () => props.reactionRole.is_system_message === true
+          )),
+        },
+        embed: {
+          required: helpers.withMessage("The message is required", requiredIf(
+            () => props.reactionRole.is_system_message === true
+          )),
+          isValidJson: helpers.withMessage("The message is not valid JSON", (value) => {
+            return !value || isValidJson(value);
+          }),
+        },
+      };
+
+      return validation_rules;
+    })
+
+    const v$ = useVuelidate(validation_rules, props.reactionRole, { $rewardEarly: true });
+
+    return {
+      FormTypeChannelListCategories,
+      FormTypeEmbed,
+      FormTypeText,
+
+      onboardingStep,
+      showOnboardingLoading,
+
+      v$,
+    };
+  },
+
+  methods: {
+    async validateOnboardingStepZero() {
+      const validForm = await this.v$.$validate();
+
+      if (!validForm) {
+        this.$store.dispatch("createToast", getValidationToast());
+        navigateToErrors();
+
+        return;
+      }
+
+      if (this.$props.reactionRole.message_link) {
+        this.showOnboardingLoading = true;
+        const messageLinkParts = this.$props.reactionRole.message_link.split("/");
+
+        this.$props.reactionRole.channel_id = messageLinkParts[messageLinkParts.length - 2];
+        this.$props.reactionRole.message_id = messageLinkParts[messageLinkParts.length - 1];
+
+        dashboardAPI.doPost(
+          endpoints.EndpointCheckMessage(this.$store.getters.getSelectedGuildID, this.$props.reactionRole.channel_id, this.$props.reactionRole.message_id),
+          {},
+          null,
+          () => {
+            this.showOnboardingLoading = false;
+            this.onboardingStep = 1;
+          },
+          (error) => {
+            this.$store.dispatch("createToast", getErrorToast(error));
+          }
+        )
+      } else {
+        this.onboardingStep = 1;
+      }
+    },
+    validateOnboardingStepOne() {
+      this.onboardingStep = 2;
+    },
+  }
+};
+</script>
