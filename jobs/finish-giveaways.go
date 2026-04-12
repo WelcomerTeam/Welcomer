5cc04f1eea5cd4d3d30c01dbefa04ea100b193c4
Blane Thompson <rock@welcomer.gg>
2026-04-12 15:23:50 +0000

    feat: Added finish giveaway job

---
+++ jobs/finish-giveaways.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"runtime/debug"
+	"time"
+
+	"github.com/WelcomerTeam/Discord/discord"
+	sandwich_protobuf "github.com/WelcomerTeam/Sandwich-Daemon/proto"
+	"github.com/WelcomerTeam/Welcomer/welcomer-core"
+	"github.com/WelcomerTeam/Welcomer/welcomer-core/database"
+	"github.com/jackc/pgx/v4"
+	_ "github.com/joho/godotenv/autoload"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	var err error
+
+	loggingLevel := flag.String("level", os.Getenv("LOGGING_LEVEL"), "Logging level")
+
+	postgresURL := flag.String("postgresURL", os.Getenv("POSTGRES_URL"), "Postgres connection URL")
+	sandwichGRPCHost := flag.String("sandwichGRPCHost", os.Getenv("SANDWICH_GRPC_HOST"), "GRPC Address for the Sandwich Daemon service")
+
+	proxyAddress := flag.String("proxyAddress", os.Getenv("PROXY_ADDRESS"), "Address to proxy requests through. This can be 'https://discord.com', if one is not setup.")
+	proxyDebug := flag.Bool("proxyDebug", false, "Enable debugging requests to the proxy")
+
+	webhookUrl := flag.String("webhookUrl", os.Getenv("JOB_CLEANUP_CUSTOM_BOTS_WEBHOOK_URL"), "Webhook URL for logging")
+
+	sandwichManagerName := flag.String("sandwichManagerName", os.Getenv("SANDWICH_MANAGER_NAME"), "Sandwich manager identifier name")
+
+	flag.Parse()
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered from panic:", r)
+			println(string(debug.Stack()))
+
+			err = welcomer.SendWebhookMessage(ctx, *webhookUrl, discord.WebhookMessageParams{
+				Content: "<@143090142360371200>",
+				Embeds: []discord.Embed{
+					{
+						Title:       "Finish Giveaways Job",
+						Description: fmt.Sprintf("Recovered from panic: %v", r),
+						Color:       int32(16760839),
+						Timestamp:   new(time.Now()),
+					},
+				},
+			})
+			if err != nil {
+				welcomer.Logger.Warn().Err(err).Msg("Failed to send webhook message")
+			}
+		}
+	}()
+
+	restInterface := welcomer.NewTwilightProxy(*proxyAddress)
+	restInterface.SetDebug(*proxyDebug)
+
+	welcomer.SetupDefaultManagerName(*sandwichManagerName)
+	welcomer.SetupLogger(*loggingLevel)
+	welcomer.SetupGRPCConnection(*sandwichGRPCHost,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(1024*1024*1024)), // Set max message size to 1GB
+	)
+	welcomer.SetupRESTInterface(restInterface)
+	welcomer.SetupSandwichClient()
+	welcomer.SetupDatabase(ctx, *postgresURL)
+
+	entrypoint(ctx, *webhookUrl)
+
+	if err := welcomer.Queries.UpsertJobCheckpoint(ctx, database.UpsertJobCheckpointParams{
+		JobName:         "finish-giveaways",
+		LastProcessedTs: time.Now().UTC(),
+	}); err != nil {
+		welcomer.Logger.Error().Err(err).Msg("Failed to upsert job checkpoint")
+	}
+
+	cancel()
+}
+
+func entrypoint(ctx context.Context, webhookUrl string) {
+	expiredGiveaways, err := welcomer.Queries.GetExpiredGiveaways(ctx)
+	if err != nil && !errors.Is(err, pgx.ErrNoRows) {
+		welcomer.Logger.Error().Err(err).Msg("Failed to fetch expired giveaways")
+
+		panic(err)
+	}
+
+	for _, giveaway := range expiredGiveaways {
+		locationsPb, err := welcomer.SandwichClient.WhereIsGuild(ctx, &sandwich_protobuf.WhereIsGuildRequest{
+			GuildId: giveaway.GuildID,
+		})
+		if err != nil {
+			welcomer.Logger.Warn().Err(err).Int64("guild_id", giveaway.GuildID).Msg("Failed to do guild lookup for giveaway")
+
+			continue
+		}
+
+		locations := locationsPb.GetLocations()
+		if len(locations) == 0 {
+			welcomer.Logger.Warn().Int64("guild_id", giveaway.GuildID).Msg("No applications found for guild in giveaway")
+
+			continue
+		}
+
+	}
+}
--- sandwich/go.mod
+++ sandwich/go.mod
@@ -3,19 +3,19 @@ module github.com/WelcomerTeam/Welcomer/sandwich
 go 1.26
 
 require (
-	github.com/WelcomerTeam/Sandwich-Daemon v0.0.0-20260322165858-683b139b5584
-	github.com/WelcomerTeam/Welcomer/welcomer-core v0.0.0-20260318222757-aa01d01abe7b
+	github.com/WelcomerTeam/Sandwich-Daemon v0.0.0-20260412131253-edad15fd4ca4
+	github.com/WelcomerTeam/Welcomer/welcomer-core v0.0.0-20260404100502-9343e9daef36
 	github.com/joho/godotenv v1.5.1
 	github.com/prometheus/client_golang v1.23.2
-	google.golang.org/grpc v1.79.3
+	google.golang.org/grpc v1.80.0
 )
 
 require (
 	github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
-	github.com/WelcomerTeam/Discord v0.0.0-20260322115948-8040d0f1005f // indirect
+	github.com/WelcomerTeam/Discord v0.0.0-20260328112928-8804aec6aa5e // indirect
 	github.com/WelcomerTeam/Mustachvulate v1.2.1-0.20231218130351-adad26f1e96e // indirect
 	github.com/WelcomerTeam/Sandwich v0.0.0-20260322170931-61406f17b909 // indirect
-	github.com/WelcomerTeam/Subway v0.0.0-20260215223101-ec5a0f747ca9 // indirect
+	github.com/WelcomerTeam/Subway v0.0.0-20260329205304-067d25428ccb // indirect
 	github.com/WelcomerTeam/czlib v0.0.0-20210907121728-d7ed7721c904 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
@@ -36,9 +36,9 @@ require (
 	github.com/jackc/puddle v1.3.0 // indirect
 	github.com/klauspost/compress v1.18.5 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-isatty v0.0.21 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
-	github.com/nats-io/nats.go v1.49.0 // indirect
+	github.com/nats-io/nats.go v1.50.0 // indirect
 	github.com/nats-io/nkeys v0.4.15 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
@@ -46,13 +46,13 @@ require (
 	github.com/prometheus/client_model v0.6.2 // indirect
 	github.com/prometheus/common v0.67.5 // indirect
 	github.com/prometheus/procfs v0.20.1 // indirect
-	github.com/rs/zerolog v1.34.0 // indirect
+	github.com/rs/zerolog v1.35.0 // indirect
 	github.com/savsgio/gotils v0.0.0-20250924091648-bce9a52d7761 // indirect
 	go.yaml.in/yaml/v2 v2.4.4 // indirect
-	golang.org/x/crypto v0.49.0 // indirect
-	golang.org/x/net v0.52.0 // indirect
-	golang.org/x/sys v0.42.0 // indirect
-	golang.org/x/text v0.35.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20260319201613-d00831a3d3e7 // indirect
+	golang.org/x/crypto v0.50.0 // indirect
+	golang.org/x/net v0.53.0 // indirect
+	golang.org/x/sys v0.43.0 // indirect
+	golang.org/x/text v0.36.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20260406210006-6f92a3bedf2d // indirect
 	google.golang.org/protobuf v1.36.11 // indirect
 )
