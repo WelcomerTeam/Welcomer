e560cb76e9081b4bb48664382fe5e78b4fd30f05
Blane Thompson <rock@welcomer.gg>
2026-07-05 19:30:23 +0000

    fix: Updated message reference

--- website/app/src/pages/dashboard/engagement/Polls.vue
+++ website/app/src/pages/dashboard/engagement/Polls.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="dashboard-contents p-8 bg-secondary-light rounded-lg text-center">
       Creating and viewing polls are not available on the dashboard at the moment.
-      <br/> Use the <kbd class="bg-secondary-dark px-2 py-1 rounded-md">/poll</kbd> commands to create and manage polls within discord.
+      <br/> Use the <kbd class="bg-secondary-dark px-2 py-1 rounded-md">/polls</kbd> commands to create and manage polls within discord.
     </div>
   </div>
 </template>
